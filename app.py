687a3005f5b9dc5489b972c3f1511bd11bfebc07
mvincentmiller <m.vincent83@gmail.com>
2017-01-15 17:05:50 -0700

    sqlite, webassets

---
+++ app.py
@@ -0,0 +1,85 @@
+# from __future__ import unicode_literals
+import os
+from builtins import str
+import sqlite3 as sql
+from flask import Flask, render_template, request, jsonify
+from flask.ext import assets
+
+app = Flask(__name__)
+
+env = assets.Environment(app)
+
+env.load_path = [
+    os.path.join(os.path.dirname(__file__), 'sass'),
+    os.path.join(os.path.dirname(__file__), 'bower_components'),
+    os.path.join(os.path.dirname(__file__ ), 'js')
+]
+
+
+sass = assets.Bundle('*.sass', filters='sass', output='sass.css')
+scss = assets.Bundle('*.scss', filters='scss', output='scss.css')
+
+env.register(
+    'css_all',
+    assets.Bundle(   scss,
+                     sass,
+                     filters='cssmin',
+                     output="all.css")
+    )
+
+env.register(
+    'js_all',
+    assets.Bundle(
+        'jquery/dist/jquery.min.js',
+        'all.js',
+        output='all.js'
+    )
+)
+
+@app.route('/')
+def home():
+   con = sql.connect("demo.db")
+   con.row_factory = sql.Row
+   cur = con.cursor()
+   cur.execute("select * from students")
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
+
+@app.route('/enternew')
+def new_student():
+   return render_template('new.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+
+         with sql.connect("demo.db") as con:
+            c = con.cursor()
+            c.execute("INSERT INTO students VALUES (?,?,?,?)", (nm,addr,city,pin) )
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+
+
+
+
+
+         # CREATE SQLITE TABLE
+         # >>> import sqlite3
+         # >>> conn = sqlite3.connect('demo.db')
+         # >>> conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+         # <sqlite3.Cursor object at 0x7fddfc07a1f0>
+
+         #export FLASK_APP=app.py
---
+++ js/all.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+$('body').hide(1000);
+$('body').fadeIn(1000);
+});
